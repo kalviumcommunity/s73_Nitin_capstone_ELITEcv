63b2a7256d313b1f4bc6e6ca035f0471fe12a691
legit92 <nitindhake3@gmail.com>
2025-04-09 15:34:03 +0530

    get_api_done

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log(err));
 
